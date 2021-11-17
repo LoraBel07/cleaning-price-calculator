bccf9956fc7aacd27aabec0e8e31e3aed60a7cbb
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-17 17:18:15 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -16,17 +16,37 @@ function sum() {
 	let box1 = $('#box1');
 	let box2 = $('#box2');
 
-	if (box1.prop('checked')) {
-		box1.val(2);
-	} else {
-		box1.val(0);
-	}
-	if (box2.prop('checked')) {
-		box2.val(1);
-	} else {
-		box2.val(0);
-	}
+	// 1й способ
 
-	let sum = (price - box1.val() - box2.val()) * area;
+	// if (box1.prop('checked')) {
+	// 	box1.val(2);
+	// } else {
+	// 	box1.val(0);
+	// }
+	// if (box2.prop('checked')) {
+	// 	box2.val(3);
+	// } else {
+	// 	box2.val(0);
+	// }
+
+	// let sum = (price - box1.val() - box2.val()) * area;
+	// console.log(sum);
+
+	// 2й спсоб
+
+	// let total = 0;
+	// $('#logistic input:checked').each(function() {
+	// 	total += parseInt($(this).val(), 0);
+	// })
+	// let sum = (price - total) * area;
+	// console.log(sum);
+		
+	// 3й спсоб
+	
+
+
+
+	
+	let sum = (price - total) * area;
 	console.log(sum);
 }
