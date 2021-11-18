e24679c2dfba0f473b1551fe1cbc26431268322f
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2021-11-18 13:00:29 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -19,17 +19,18 @@ function sum() {
 	// 1й способ
 
 	if (box1.prop('checked')) {
-		box1.val(1);
+		box1.val(1000);
 	} else {
 		box1.val(0);
 	}
 	if (box2.prop('checked')) {
-		box2.val(50);
+		box2.val(5);
 	} else {
 		box2.val(0);
 	}
 
 	let sum = (price + box1.val() + box2.val()) * area;
+	sum = Math.floor(sum);
 	$('.sum').text(sum);
 
 	// 2й спсоб
